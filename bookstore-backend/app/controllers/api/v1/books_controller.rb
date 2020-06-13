e368a4952261fe3e893a4818118838fea1dad29a
jon <jonathan.wong110@gmail.com>
2020-06-13 14:11:10 -0400

    created methods for the books and reviews controller

--- bookstore-backend/app/controllers/api/v1/books_controller.rb
+++ bookstore-backend/app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::BooksController < ApplicationController
+    before_action :set_product, only: [:show, :update, :destroy]
+
+    def index
+      @books = Book.all
+
+      render json: @books, status: 200
+    end
+
+    def show
+      render json: @book, status: 200
+    end
+
+    private
+      def set_product
+        @book = Book.find(params[:id])
+      end
+
+      def product_params
+        params.require(:book).permit(:title, :price, :description, :image, :category)
+      end
 end
