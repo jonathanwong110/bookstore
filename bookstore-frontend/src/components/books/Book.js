85f5e005a2fab7b6f5859595c7086e4600e94742
jon <jonathan.wong110@gmail.com>
2020-06-14 00:44:30 -0400

    created a showBook component to show more details of a book

--- bookstore-frontend/src/components/books/Book.js
+++ bookstore-frontend/src/components/books/Book.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Card } from 'react-bootstrap';
+import ShowBook from './ShowBook'
 
 export default function Book(props) {
 
     const { book } = props
 
     return (
-        <Card style={{ width: '10rem', height: '17rem', margin: '10px' }}>
+        <Card style={{ width: '10rem', height: '22rem', margin: '10px' }}>
             <Card.Img variant="top" src={book.image} height="200px" width="200px" />
             <Card.Body>
                 {/* <a href="/books" class="title">{book.title}</a> */}
                 <a href="/books" className="title">{book.title}</a>
+                <br></br>
+                <ShowBook books={props} />
             </Card.Body>
         </Card>
     )
---
+++ bookstore-frontend/src/components/books/ShowBook.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function DisplayBook(props) {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const { books } = props
+    if (!books) return null
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Details
+      </Button>
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        <img
+                            alt={books.book.id} src={books.book.image} height="300px" width="200px">
+                        </img>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h2>{books.book.title}</h2>
+                    by
+                    <br></br>
+                    <br></br>
+                    <h6>{books.book.author}</h6>
+                    <br></br>
+                    <h4>${books.book.price}.00</h4>
+                    <br></br>
+                    <p>{books.book.description}</p>
+                    <h3>Reviews: </h3>
+                    {books.book.reviews && books.book.reviews.length > 0 ?
+                        books.book.reviews.map(review => {
+                            return (
+                                <ul key={review.id}>
+                                    <li>
+                                        {review.content}
+                                    </li>
+                                </ul>
+                            )
+                        })
+                        :
+                        <>
+                            <p>
+                                No Reviews so far!
+                </p>
+                        </>
+                    }
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="dark" onClick={handleClose}>
+                        Close
+          </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default DisplayBook
